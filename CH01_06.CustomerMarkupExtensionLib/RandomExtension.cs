70f79bd341b280bcea26746d3adb529907da39af
VdPConsultants <jan.vandenpoel@vdpc.be>
2020-08-25 22:33:12 +0200

    Chapter 01

---
+++ CH01_06.CustomerMarkupExtensionLib/RandomExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace CH01_06.CustomMarkupExtensionLib
+{
+    public class RandomExtension : MarkupExtension
+    {
+        readonly int _from, _to;
+        static readonly Random _rnd = new Random();
+        public RandomExtension(int from, int to)
+        {
+            _from = from; _to = to;
+        }
+        public RandomExtension(int to) : this(0, to)
+        {
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return (double)_rnd.Next(_from, _to);
+        }
+    }
+}
