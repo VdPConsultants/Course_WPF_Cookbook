ab8e375a95821c1a151d6caa3a6edaac7aa01182
VdPConsultants <jan.vandenpoel@vdpc.be>
2020-08-26 16:24:58 +0200

    Chapter 04

---
+++ CH04_02.ContentControls/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH04_02.ContentControls
+{
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0} is {1} years old", Name, Age);
+        }
+    }
+}
